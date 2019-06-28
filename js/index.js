a9b5eebe3482838222319587c8397081f3753017
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-28 18:41:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@
 function greet() {
     console.log(`my name is ${this.name}, hi!`);
 }
- 
+
 let sally = { name: 'Sally' };
- 
+
 greet.call(sally);
 // my name is Sally, hi!
- 
+
 greet.apply(sally);
 // my name is Sally, hi!
