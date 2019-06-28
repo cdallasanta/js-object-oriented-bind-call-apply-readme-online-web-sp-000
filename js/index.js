e432c59e23ad5d06e610e55e6b56366d7e4e39f4
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-28 18:46:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-// use this to copy code snippets or use your browser's console
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+class Event {
+  constructor(title, keywords) {
+    this.title = title;
+    this.keywords = keywords;
+  }
 }
+ 
+class User {
+  constructor(name, interests) {
+    this.name = name;
+    this.interests = interests;
+  }
 
-let sally = { name: 'Sally' };
-
-greet.call(sally, 'Terry', 'George');
-// my name is Sally, hi!
-
-greet.apply(sally, ['Terry', 'George']);
-// my name is Sally, hi!
+  matchInterests(event) {
+    return event.keywords.some(function(word) {
+      return this.interests.includes(word);
+    });
+  }
+}
